bfe461c11d59f55a624886777c90beb3c6eb19e4
Carmine Calabrese <carmine.calabrese99@gmail.com>
2024-06-20 16:45:00 +0200

    Collected photo for 30/60/90 Speed Limiter. Added Fog condition for every sign

--- Script/carla_data_collection.py
+++ Script/carla_data_collection.py
@@ -8,7 +8,6 @@ import re
 import shutil
 
 
-# Assicurati di avere CARLA nel percorso
 try:
     sys.path.append(
         glob.glob(
@@ -31,8 +30,7 @@ IMG_HEIGHT = 480
 TOTAL_PHOTOS = 0 
 
 
-
-directory_name = "output_data\\duststorm"
+directory_name = "output_data\\stop_clearnight"
 current_dir = os.getcwd()
 if os.path.exists(directory_name):
     shutil.rmtree(directory_name)
@@ -51,7 +49,7 @@ def get_yaw_from_vector(v):
 def salva_immagine(image):
         global TOTAL_PHOTOS
         TOTAL_PHOTOS += 1
-        image.save_to_disk(IMG_FOLDER + "STOP_DUSTSTORM_%d" % TOTAL_PHOTOS)
+        image.save_to_disk(IMG_FOLDER + "30_CLEARNIGHT_%d" % TOTAL_PHOTOS)
 
 def scatta_foto(sensor):
     global TOTAL_PHOTOS
@@ -127,6 +125,17 @@ def main():
         action='store_true',
         help='Change engine quality'
     )
+    parser.add_argument(
+        '--spectator_cords',
+        action='store_true',
+        help='Start printing spectator cords'
+    )
+    parser.add_argument(
+        '--vehicle',
+        action='store_true',
+        help='Spawn vehicle with camera and start taking photo'
+    )
+    
         
     args = parser.parse_args() 
     
@@ -157,44 +166,53 @@ def main():
                 for x in dir(carla.WeatherParameters)
                 if re.match("[A-Z].+", x)
             }
-            if args.weather in weather_presets:
-                world.set_weather(weather_presets[args.weather])
-                print(f"Setting the weather to {args.weather}")
-            else:
-                print(f"Weather preset '{args.weather}' not found. Using default weather.")
+            
             settings.no_rendering_mode = False
             world.apply_settings(settings)
+            spectator = world.get_spectator()
             if args.spectator:
                 print("Moving spectator to default position...")
                 spectator = world.get_spectator()
                 spectator.set_transform(
                     carla.Transform(carla.Location(x=569.050476, y=3506.865967, z=370.496704)),
                 )
+            if args.spectator_cords:
+                while(True):
+                    print(spectator.get_transform())
+            
         else:
             world = client.get_world()
 
-        vehicle_bp = world.get_blueprint_library().find("vehicle.tesla.model3")
-        spawn_point = carla.Transform(
-            carla.Location(x=569.050476, y=3511.865967, z=370.496704), #x=569.050476, y=3501.865967, z=370.496704
-            carla.Rotation(yaw=-90), #90
-        )
-        vehicle = world.spawn_actor(vehicle_bp, spawn_point)
-        vehicle.apply_control(carla.VehicleControl(throttle=0.2, steer=0.0))
-        # print("Vehicle spawned at:", vehicle.get_transform())
-        # time.sleep(7)
-        
-        
-        print("Attaching camera...")
-        camera_bp = world.get_blueprint_library().find('sensor.camera.rgb')
-        camera_bp.set_attribute('image_size_x', f"{IMG_WIDTH}")
-        camera_bp.set_attribute('image_size_y', f"{IMG_HEIGHT}")
-        camera_bp.set_attribute('fov', '30')
 
-        
-        camera_transform = carla.Transform(carla.Location(x=1, z=2, y= 1.6), carla.Rotation(yaw=20)) #x=1.5, z=2.4 
-        camera_sensor = world.spawn_actor(camera_bp, camera_transform, attach_to=vehicle)
-        scatta_foto(camera_sensor)
-        time.sleep(22)
+        if args.vehicle:
+            vehicle_bp = world.get_blueprint_library().find("vehicle.tesla.model3")
+            spawn_point_12 = carla.Transform(
+                carla.Location(x=569.050476, y=3511.865967, z=370.496704), #x=569.050476, y=3501.865967, z=370.496704
+                carla.Rotation(yaw=-90), #90
+            )
+            #X=160 => 90 | X=60 => 60 | X=35 => 30
+            spawn_point_02 = carla.Transform(
+                carla.Location(x=35, y=106, z=2), #x=569.050476, y=3501.865967, z=370.496704
+                carla.Rotation(yaw=180), #90
+            )
+            vehicle = world.spawn_actor(vehicle_bp, spawn_point_12)
+            vehicle.apply_control(carla.VehicleControl(throttle=0.15, steer=0.0))
+            # print("Vehicle spawned at:", vehicle.get_transform())
+            time.sleep(3)
+            
+            print("Attaching camera...")
+            camera_bp = world.get_blueprint_library().find('sensor.camera.rgb')
+            camera_bp.set_attribute('image_size_x', f"{IMG_WIDTH}")
+            camera_bp.set_attribute('image_size_y', f"{IMG_HEIGHT}")
+            camera_bp.set_attribute('fov', '30')
+
+            
+            camera_transform = carla.Transform(carla.Location(x=1, z=2, y= 1.6), carla.Rotation(yaw=15)) #x=yaw=20 for Town12
+            camera_sensor = world.spawn_actor(camera_bp, camera_transform, attach_to=vehicle)
+            scatta_foto(camera_sensor)
+        time.sleep(25)
+        # while(True):
+        #     continue
         
     except Exception as e:
         print(f"An error occurred: {e}")
