22502f32bccb4193d518bbde3d8d85a12ff92f6e
Carmine Calabrese <carmine.calabrese99@gmail.com>
2024-06-20 13:23:49 +0200

    Completed Script and added the STOP_SIGN part of the dataset

--- Script/carla_data_collection.py
+++ Script/carla_data_collection.py
@@ -2,10 +2,11 @@ import glob
 import math
 import os
 import sys
-import random
 import time
 import argparse
 import re
+import shutil
+
 
 # Assicurati di avere CARLA nel percorso
 try:
@@ -21,31 +22,41 @@ try:
     )
 except IndexError:
     pass
+
 import carla
 
 IMG_FOLDER = ""
 IMG_WIDTH = 480
 IMG_HEIGHT = 480
-TOTAL_PHOTOS = 0
+TOTAL_PHOTOS = 0 
 
-def gen_output_dir():
-    directory_name = "output_data"
-    current_dir = os.getcwd()
-    if os.path.exists(directory_name):
-        os.rmdir(directory_name)
-        print(f"Directory '{directory_name}' deleted")
-        os.makedirs(directory_name)
-        print(f"Directory '{directory_name}' created")
-    else:
-        os.makedirs(directory_name)
-        print(f"Directory '{directory_name}' already exists in '{current_dir}'")
-    
-    IMG_FOLDER = f"{current_dir}\\{directory_name}\\"
-    print(IMG_FOLDER)
-        
+
+
+directory_name = "output_data\\duststorm"
+current_dir = os.getcwd()
+if os.path.exists(directory_name):
+    shutil.rmtree(directory_name)
+    print(f"Directory '{directory_name}' deleted")
+    os.makedirs(directory_name)
+    print(f"Directory '{directory_name}' created")
+else:
+    os.makedirs(directory_name)
+    print(f"Directory '{directory_name}' already exists in '{current_dir}'")
+
+IMG_FOLDER = f"{current_dir}\\{directory_name}\\"
 
 def get_yaw_from_vector(v):
     return math.degrees(math.atan2(v.y, v.x))
+
+def salva_immagine(image):
+        global TOTAL_PHOTOS
+        TOTAL_PHOTOS += 1
+        image.save_to_disk(IMG_FOLDER + "STOP_DUSTSTORM_%d" % TOTAL_PHOTOS)
+
+def scatta_foto(sensor):
+    global TOTAL_PHOTOS
+    sensor.listen(salva_immagine)
+    
 
 def main():
     parser = argparse.ArgumentParser(description="CARLA Data Collection Script")
@@ -62,7 +73,7 @@ def main():
     )
     parser.add_argument(
         "--map",
-        default="Town12",
+        action="store_true",
         help="Load a new map, use --list to see available maps",
     )
     parser.add_argument(
@@ -72,18 +83,6 @@ def main():
     )
     parser.add_argument(
         "--res", default="480x480", help="Resolution of the camera (default: 480x480)"
-    )
-    parser.add_argument(
-        "--fps",
-        default=0,
-        type=float,
-        help="Set fixed FPS, zero for variable FPS (default: 30)",
-    )
-    parser.add_argument(
-        "--delta-seconds",
-        default=0.05,
-        type=float,
-        help="Set fixed delta seconds, zero for variable frame rate (default: 0.05)",
     )
     parser.add_argument(
         "--timeout",
@@ -109,9 +108,24 @@ def main():
         help='reload current map'
     )
     parser.add_argument(
-        '-f', '--first',
+        '-p', '--params',
         action='store_true',
         help='load the entire map'
+    )
+    parser.add_argument(
+        '--spectator',
+        action='store_true',
+        help='move the spectator to the default position '
+    )
+    parser.add_argument(
+        '--sign',
+        default="",
+        help="Enable camera sensor (default: True)",
+    )
+    parser.add_argument(
+        '--quality',
+        action='store_true',
+        help='Change engine quality'
     )
         
     args = parser.parse_args() 
@@ -122,36 +136,21 @@ def main():
     client.set_timeout(args.timeout)
 
     try:
-        if args.first:
-            print("First map loading")
+        if args.params:
+            print("Loading with parameters...")
             if args.reload_map:
-                print("Reloading Map")
+                print("Reloading Map...")
                 world = client.reload_world()
-            elif args.map is not None:
-                world = client.load_world(args.map)
-                print(f"Mondo creato '{args.map}'")
-            
+            elif args.map:
+                map_input = input("Insert the map to load: ")
+                print(f"Loading Map '{map_input}'...")
+                world = client.load_world(map_input)
+                
             world = client.get_world()
             time.sleep(5)
             settings = world.get_settings()
-
-            if args.delta_seconds is not None:
-                settings.fixed_delta_seconds = args.delta_seconds
-            elif args.fps is not None:
-                settings.fixed_delta_seconds = (1.0 / args.fps) if args.fps > 0.0 else 0.0
-
-            if args.delta_seconds is not None or args.fps is not None:
-                if settings.fixed_delta_seconds > 0.0:
-                    print(
-                        "set fixed frame rate %.2f milliseconds (%d FPS)"
-                        % (
-                            1000.0 * settings.fixed_delta_seconds,
-                            1.0 / settings.fixed_delta_seconds,
-                        )
-                    )
-                else:
-                    print("set variable frame rate.")
-                    settings.fixed_delta_seconds = None
+            if args.quality:
+                settings.quality_level = carla.QualityLevel.High
 
             weather_presets = {
                 x: getattr(carla.WeatherParameters, x)
@@ -160,43 +159,42 @@ def main():
             }
             if args.weather in weather_presets:
                 world.set_weather(weather_presets[args.weather])
+                print(f"Setting the weather to {args.weather}")
             else:
                 print(f"Weather preset '{args.weather}' not found. Using default weather.")
-
-            # settings.fixed_delta_seconds = args.delta_seconds
-            # settings.synchronous_mode = True  # Attiva la modalità sincrona
             settings.no_rendering_mode = False
             world.apply_settings(settings)
+            if args.spectator:
+                print("Moving spectator to default position...")
+                spectator = world.get_spectator()
+                spectator.set_transform(
+                    carla.Transform(carla.Location(x=569.050476, y=3506.865967, z=370.496704)),
+                )
         else:
             world = client.get_world()
-        # Aggiungi il punto di spawn per lo spettatore
-        spectator = world.get_spectator()
-        # spectator.set_transform(
-        #     carla.Transform(carla.Location(x=569.050476, y=3506.865967, z=370.496704)),
-        # )
 
-        # Ottieni il veicolo
         vehicle_bp = world.get_blueprint_library().find("vehicle.tesla.model3")
         spawn_point = carla.Transform(
-            carla.Location(x=569.050476, y=3501.865967, z=370.496704),
+            carla.Location(x=569.050476, y=3511.865967, z=370.496704), #x=569.050476, y=3501.865967, z=370.496704
             carla.Rotation(yaw=-90), #90
         )
         vehicle = world.spawn_actor(vehicle_bp, spawn_point)
+        vehicle.apply_control(carla.VehicleControl(throttle=0.2, steer=0.0))
         # print("Vehicle spawned at:", vehicle.get_transform())
-        # time.sleep(10)
-        spectator_speed = args.spectator_speed
-        gen_output_dir()
+        # time.sleep(7)
+        
+        
         print("Attaching camera...")
         camera_bp = world.get_blueprint_library().find('sensor.camera.rgb')
         camera_bp.set_attribute('image_size_x', f"{IMG_WIDTH}")
         camera_bp.set_attribute('image_size_y', f"{IMG_HEIGHT}")
-        camera_transform = carla.Transform(carla.Location(x=1, z=2, y= 1)) #x=1.5, z=2.4 
-        camera_sensor = world.spawn_actor(camera_bp, camera_transform, attach_to=vehicle)
+        camera_bp.set_attribute('fov', '30')
 
-        vehicle.set_autopilot(True)
-        # print(f"Frame: {camera_sensor.frame}")
-        camera_sensor.listen(lambda data: data.save_to_disk(f"{IMG_FOLDER}stop_{data.frame}"))
-        time.sleep(60) # TEMPO DI ESECUZIONE IN SECONDI
+        
+        camera_transform = carla.Transform(carla.Location(x=1, z=2, y= 1.6), carla.Rotation(yaw=20)) #x=1.5, z=2.4 
+        camera_sensor = world.spawn_actor(camera_bp, camera_transform, attach_to=vehicle)
+        scatta_foto(camera_sensor)
+        time.sleep(22)
         
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -207,6 +205,7 @@ def main():
                 print(f"Destroying '{camera_sensor}'")
                 camera_sensor.stop()
                 camera_sensor.destroy()
+            
         except:
             pass
         try:
