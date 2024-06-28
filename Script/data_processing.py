bdb068d52db5d4f862c3ff485ade210d4bfd4dd4
Carmine Calabrese <carmine.calabrese99@gmail.com>
2024-06-28 11:20:38 +0200

    Changes to be committed: 	modified:   .gitignore 	modified:   Script/data_processing.py

--- Script/data_processing.py
+++ Script/data_processing.py
@@ -11,26 +11,27 @@ from keras.utils import image_utils as image
 img_width, img_height = 480, 480
 
 #PATHS
-base_folder = "..\..\Assets"
-# folders = [f'{base_path}\CROPPED_Dataset\SPEED_LIMITER_30',f'{base_path}\CROPPED_Dataset\SPEED_LIMITER_60',f'{base_path}\CROPPED_Dataset\SPEED_LIMITER_90',f'{base_path}\CROPPED_Dataset\STOP_SIGN',]
+base_folder = "..\Assets"
+
+#TRAIN/VALIDATION Datesets
 dataset_folder = f"{base_folder}\CROPPED_Dataset\\"
 output_folder = f'{base_folder}\PROCESSED_Dataset\\'
 
+#TESTING Datasets
+dataset_testing_folder = f'{base_folder}\RAW_TESTING_Dataset\\'
+output_testing_folder = f'{base_folder}\PROCESSED_TESTING_Dataset\\'
+
 #IMAGE PRE-PROCESSING
 def preprocess_image(image_path):
-    #Loading Image
-    img = image.load_img(image_path, target_size=(img_width, img_height))
 
-    #Conversion to a Numpy array
+    img = image.load_img(image_path, target_size=(img_width, img_height))
     img_array = image.img_to_array(img)
 
-    #Image Normalization
     img_tensor = np.expand_dims(img_array, axis=0)
     img_tensor /= 255.0
 
     return img_tensor
 
-#Save the Tensor in a npy file into the "output_path"
 def save_tensor(tensor, output_path):
     np.save(output_path , tensor)
 
@@ -40,14 +41,18 @@ def get_all_subfolders(directory):
         for dir in dirs:
             subfolders.append(os.path.join(root, dir))
     return subfolders
-    
-if not os.path.exists(output_folder):    
+
+if not os.path.exists(output_testing_folder):    
     print("Creating output folder...")   
-    os.makedirs(output_folder)
+    os.makedirs(output_testing_folder)
+    print(f'Created {output_testing_folder}...')
+else:
+    print("Output folder already exists.")
 
-dataset_folder = [element for element in get_all_subfolders(dataset_folder) if 'Padding' in element]
-
+# dataset_folder = [element for element in get_all_subfolders(dataset_folder) if 'Padding' in element]
+dataset_folder = get_all_subfolders(dataset_testing_folder)
 for folder in dataset_folder:
+    print(f"Processing {folder}...")
     if "STOP" in folder:
         class_name = "STOP_SIGN"
     elif "30" in folder:
@@ -57,7 +62,7 @@ for folder in dataset_folder:
     elif "90" in folder:
         class_name = "SPEED_LIMITER_90"    
     
-    output_class_folder = os.path.join(output_folder, class_name)
+    output_class_folder = os.path.join(output_testing_folder, class_name)
         
     if not os.path.exists(output_class_folder):
         os.makedirs(output_class_folder)
